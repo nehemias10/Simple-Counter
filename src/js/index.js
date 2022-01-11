06d19c650b0360433a89292769a5a3056d5ffc6c
nehemias10 <nehemiasrivero1@gmail.com>
2022-01-11 01:33:36 +0000

    uno

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,23 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+/* ReactDOM.render(<Home />, document.querySelector("#app")); */
+import { SecondsCounter } from "./component/home.js";
+
+let counter = 0;
+setInterval(function () {
+	ReactDOM.render(
+		<SecondsCounter seconds={counter} />,
+		document.getElementById("app")
+	);
+	counter += 1;
+}, 1000);
